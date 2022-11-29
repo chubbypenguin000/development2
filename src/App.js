d7b8d55669179f46111feff29f683e79a2025c3b
kathy <kathy_li@brown.edu>
2022-11-29 16:24:31 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
   const [isCalathea, setIsCalathea] = useState(false);
   const [isCactus, setIsCactus] = useState(false);
   const [isPothos, setIsPothos] = useState(false);
+
+  const [isFour, setIsFour] = useState(false);
+  const [isSix, setIsSix] = useState(false);
+  const [isEight, setIsEight] = useState(false);
+
   const [cartItems, setCartItems] = useState([]);
   const [total, setTotal] = useState(0);
 
@@ -31,6 +36,18 @@ function App() {
       sortedPlants = sortedPlants.filter((plant) => plant.type === "pothos");
     }
 
+    if (isFour) {
+      sortedPlants = sortedPlants.filter((plant) => plant.pot === "4");
+    }
+
+    if (isSix) {
+      sortedPlants = sortedPlants.filter((plant) => plant.pot === "6");
+    }
+
+    if (isEight) {
+      sortedPlants = sortedPlants.filter((plant) => plant.pot === "8");
+    }
+
     if (isSorted) {
       sortedPlants = [...sortedPlants].sort((a, b) => {
         return a.cost > b.cost ? 1 : -1;
@@ -38,7 +55,7 @@ function App() {
     }
 
     setPlants(sortedPlants);
-  }, [isSorted, isCalathea, isCactus, isPothos]);
+  }, [isSorted, isCalathea, isCactus, isPothos, isFour, isSix, isEight]);
 
   const handleReset = () => {
     setCartItems([]);
@@ -49,6 +66,9 @@ function App() {
     setIsCalathea(false);
     setIsCactus(false);
     setIsPothos(false);
+    setIsFour(false);
+    setIsSix(false);
+    setIsEight(false);
   }
 
   const handleSort = () => {
@@ -92,6 +112,12 @@ function App() {
       setIsCactus(!isCactus);
     } else if (type === "pothos") {
       setIsPothos(!isPothos);
+    } else if (type === "4") {
+      setIsFour(!isFour);
+    }  else if (type === "6") {
+      setIsSix(!isSix);
+    } else if (type === "8") {
+      setIsEight(!isEight);
     }
   };
 
@@ -139,9 +165,13 @@ function App() {
           </div>
 
           {/* FILTER BY */}
+          {/* plant type */}
           <h2 className="mt-10 text-green-900 text-3xl font-bold flex flex-col">
             filter by:
           </h2>
+          <h3 className="mt-2 text-green-900 text-xl font-bold flex flex-col">
+            plant type
+          </h3>
           <div class="mt-4 flex items-center mb-4">
             <input
               id="default-checkbox"
@@ -189,6 +219,60 @@ function App() {
               pothos
             </label>
           </div>
+
+
+          {/* pot size */}
+          <h3 className="mt-2 text-green-900 text-xl font-bold flex flex-col">
+            pot size
+          </h3>
+          <div class="mt-4 flex items-center mb-4">
+            <input
+              id="default-checkbox"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 dark:bg-gray-700 dark:border-gray-600"
+              onChange={() => handleType("4")}
+            />
+            <label
+              for="default-checkbox"
+              class="ml-2 text-sm font-medium text-neutral-700"
+            >
+              4"
+            </label>
+          </div>
+
+          <div class="flex items-center mb-4">
+            <input
+              id="default-checkbox"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 dark:bg-gray-700 dark:border-gray-600"
+              onChange={() => handleType("6")}
+            />
+            <label
+              for="default-checkbox"
+              class="ml-2 text-sm font-medium text-neutral-700"
+            >
+              6"
+            </label>
+          </div>
+
+          <div class="flex items-center mb-4">
+            <input
+              id="default-checkbox"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 dark:bg-gray-700 dark:border-gray-600"
+              onChange={() => handleType("8")}
+            />
+            <label
+              for="default-checkbox"
+              class="ml-2 text-sm font-medium text-neutral-700"
+            >
+              8"
+            </label>
+          </div>
+
           {/* CART TOTAL */}
           <h2 className="mt-8 mb-2 text-green-900 text-3xl font-bold flex flex-col">
             cart:
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,23 @@
 const Cart = (props) => {
-  const { cartItems, total } = props;
+  const { cartItems, total, onRemove, onAdd } = props;
   return (
     <div>
       <div>
         {cartItems.length === 0 && <div class="mt-3 text-base font-medium text-neutral-700">your cart is empty!</div>}
         {cartItems.map((item) => (
           <div key={item.name}>
-            <div class="text-base font-medium text-neutral-700">
-              {item.name} x {item.qty}
+            <div class="mb-3 text-base font-medium text-neutral-700">
+              {item.name} x{item.qty}
+              {" "}
+            <button onClick={() => onRemove(item)} class="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-2 border border-gray-400 rounded shadow">
+              -
+            </button> {" "}
+            <button onClick={() => onAdd(item)} class="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-2 border border-gray-400 rounded shadow">
+              +
+            </button>
             </div>
+              
+
           </div>
         ))}
         <div class="mt-3 text-base font-medium text-neutral-700">total cost: ${total}</div>
